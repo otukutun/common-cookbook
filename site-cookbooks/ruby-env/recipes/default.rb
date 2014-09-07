e349cb72b80fedca4b8567447440470c820b6b24
otukutun <orehaorz@gmail.com>
2014-09-07 15:45:50 +0900

    add ruby-build

--- site-cookbooks/ruby-env/recipes/default.rb
+++ site-cookbooks/ruby-env/recipes/default.rb
@@ -29,3 +29,26 @@ template ".bash_profile" do
   group node['ruby-env']['group']
   not_if "grep rbenv ~/.bash_profile", :environment => { :'HOME' => "/home/#{node['ruby-env']['user']}" }
 end
+
+
+directory "/home/#{node['ruby-env']['user']}/.rbenv/plugins" do
+  owner node['ruby-env']['user']
+  group node['ruby-env']['group']
+  mode 0755
+  action :create
+end
+
+git "/home/#{node['ruby-env']['user']}/.rbenv/plugins/ruby-build" do
+  repository node["ruby-env"]["ruby-build_url"]
+  action :sync
+  user node['ruby-env']['user']
+  group node['ruby-env']['group']
+end
+
+execute "rbenv install #{node['ruby-env']['version']}" do
+  command "/home/#{node['ruby-env']['user']}/.rbenv/bin/rbenv install #{node['ruby-env']['version']}"
+  user node['ruby-env']['user']
+  group node['ruby-env']['group']
+  environment 'HOME' => "/home/#{node['ruby-env']['user']}"
+  not_if { File.exists?("/home/#{node['ruby-env']['user']}/.rbenv/versions/#{node['ruby-env']['version']}" )}
+end
